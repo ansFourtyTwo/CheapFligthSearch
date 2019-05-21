6fb1b693f30b9f3f3277f646446e7eb5b0b2e3b0
ansFourtyTwo <simon.2.fink@gmail.com>
2019-05-21 23:00:21 +0200

    Added chromedriver and selenium modules

--- cheapFlightSearch.py
+++ cheapFlightSearch.py
@@ -1,9 +1,34 @@
-import os
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+import datetime
+
 
 def main():
-	print('Ready to find cheap fligths')
-	print('I am going to help you')
-	pass
+	browser = webdriver.Chrome(executable_path='chromedriver')
+	browser.get('https://www.expedia.com/Flights')
+	browser.
+	time.sleep(4)
+
 	
+	#Setting ticket types paths
+	return_ticket = "//label[@id='flight-type-roundtrip-label-hp-flight']"
+	one_way_ticket = "//label[@id='flight-type-one-way-label-hp-flight']"
+	multi_ticket = "//label[@id='flight-type-multi-dest-label-hp-flight']"
+
+def ticket_chooser(ticket):
+    try:
+        ticket_type = browser.find_element_by_xpath(ticket)
+        ticket_type.click()
+    except Exception as e:
+
+        pass
+
 if __name__ == "__main__":
 	main()
+
+
